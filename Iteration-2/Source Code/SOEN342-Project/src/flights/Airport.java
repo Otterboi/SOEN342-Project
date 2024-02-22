5c44c1bfbcba466ed7ea595a6c00005dbba8cf6d
Joseph <51335923+Otterboi@users.noreply.github.com>
2024-02-22 18:26:00 -0500

    Add Airport relavent code

--- Iteration-2/Source Code/SOEN342-Project/src/flights/Airport.java
+++ Iteration-2/Source Code/SOEN342-Project/src/flights/Airport.java
@@ -1,4 +1,29 @@
 package flights;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Airport {
+    private String name;
+    private String code;
+    private City city;
+    private List<Flight> listOfFlights;
+
+    public Airport(){
+        this.name = "";
+        this.code = "";
+        this.city = new City();
+        this.listOfFlights = new ArrayList<Flight>();
+    }
+
+    public Airport(String name, String code, City city, ArrayList<Flight> listOfFlights){
+        this.name = name;
+        this.code = code;
+        this.city = city;
+        this.listOfFlights = listOfFlights;
+    }
+
+    public List<Flight> getFlights(){
+        return this.listOfFlights;
+    }
 }
